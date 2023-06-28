4daab84825cca118fb7c3f8cf390a501433ec0e4
Edmund-Frio <edmundfrio@gmail.com>
2023-06-28 12:10:23 -1000

    added github and home

--- main.js
+++
@@ -1,7 +0,0 @@
-let addButton = document.getElementById("add");
-let listContainer = document.getElementById("listContainer");
-let input = document.getElementById("inputX ");
-
-addButton.addEventListener("click", function () {
-  var paragraph = document.createElement("p");
-});
