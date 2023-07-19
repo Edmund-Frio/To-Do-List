67e00920f7d3b713301864243940f8e2ce0320ca
Edmund Frio <edmundfrio@gmail.com>
2023-07-18 14:06:22 -1000

    link js to html, added js func, css changes

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-const input = document.getElementById("input")
-const todos = document.getElementById("todos")
+let inputBox = document.getElementById("input");
+let todos = document.getElementById("todos");
 
 function newToDo() {
-  
+  if (input.value === "") {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    todos.appendChild(li);
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    todos.appendChild(li);
+    let span = document.createElement("span")
+    span.innerHTML = "<i class='bx bx-x-circle'></i>"
+    li.appendChild(span);
+  }
+  input.value = ""
 }
